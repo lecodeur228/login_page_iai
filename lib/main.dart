7b7a08e0f12816c5cd0d7e738eff70219e387b98
lecodeur228 <banarin60@gmail.com>
2024-05-31 11:29:00 +0000

    add button and eyes icon

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool isObscure = true;
 
   void _incrementCounter() {
     setState(() {
@@ -49,77 +50,129 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipOval(
-              child: SizedBox(
-                height: 150,
-                width: 150,
-                child: Image.asset(
-                  "assets/logo.jpeg",
-                  fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipOval(
+                child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Image.asset(
+                    "assets/logo.jpeg",
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            // pour donner de espace methode 1
-            // const SizedBox(
-            //   height: 10,
-            // ),
-            // pour donner de espace methode 2 avec le padding
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Text(
-                "Le logo de iai depuis 2002",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
+              // pour donner de espace methode 1
+              // const SizedBox(
+              //   height: 10,
+              // ),
+              // pour donner de espace methode 2 avec le padding
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Text(
+                  "Le logo de iai depuis 2002",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.person_3_outlined,
-                    size: 35,
-                  ),
-                  Expanded(
-                      child: SizedBox(
-                          height: 50,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(12))),
-                          )))
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.person_3_outlined,
+                      size: 35,
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                        child: SizedBox(
+                            height: 50,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  labelText: "Email",
+                                  hintText: "Entrer votre mot de passe",
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12))),
+                            )))
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.password,
-                    size: 35,
-                  ),
-                  Expanded(
-                      child: SizedBox(
-                          height: 50,
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(12))),
-                          )))
-                ],
+              const SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.password,
+                      size: 35,
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                        child: SizedBox(
+                            height: 50,
+                            child: TextField(
+                              obscureText: isObscure,
+                              autocorrect: false,
+                              enableSuggestions: false,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                  labelText: "Mot de passe",
+                                  hintText: "Entrer votre mot de passe",
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isObscure = !isObscure;
+                                      });
+                                    },
+                                    icon: isObscure
+                                        ? const Icon(Icons.visibility)
+                                        : const Icon(Icons.visibility_off),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12))),
+                            ))),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                          style: const ButtonStyle(
+                              foregroundColor:
+                                  MaterialStatePropertyAll(Colors.white),
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Colors.amber)),
+                          onPressed: () {
+                            print("ok");
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              "Connexion",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
